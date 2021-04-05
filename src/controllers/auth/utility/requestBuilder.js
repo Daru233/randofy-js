85281e2f86f098a8b4ca48eaa81778fb0dbbbd08
Michael Malto <michaeldm233@gmail.com>
2021-04-05 20:02:03 +0100

    unused import clean up

--- src/controllers/auth/utility/requestBuilder.js
+++ src/controllers/auth/utility/requestBuilder.js
@@ -4,10 +4,8 @@ import axios from "axios";
 
 const CLIENT_ID = config.get("Spotify.CLIENT_ID");
 const CLIENT_SECRET = config.get("Spotify.CLIENT_SECRET");
-const scopes = 'user-read-private user-read-email';
 const redirect_uri = "http://localhost:8080/requestTokens";
 const TOKEN_URL = "https://accounts.spotify.com/api/token";
-let access_token = "";
 
 
 export const buildRequest = (code) => {
