1521177b734c1ea6c93429979f6876eeab73424d
Michael Malto <michaeldm233@gmail.com>
2021-04-19 20:53:42 +0100

    added process env port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import config from "config";
 import { app } from "./app.js";
 
-const PORT = config.get("App.PORT");
+const PORT = process.env.SERVER_PORT || config.get("App.PORT");
 
 app.listen(PORT, () => {
     console.log(`Server started at: http://localhost:${PORT}`);
